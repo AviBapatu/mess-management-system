f0d27cd760177e4c3a2413efef6900c9a67e6cde
AviBapatu <avinashreddybapatu2006@gmail.com>
2025-09-21 11:31:19 +0530

    - fixed cors config

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,13 @@ const weeklyMenuRoutes = require("./routes/weeklyMenuRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
